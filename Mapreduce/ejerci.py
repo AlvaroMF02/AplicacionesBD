faf4f28ac95b974f9818cc619161ac2b5dfcdb25
AvaroMF02 <alvaromarflo@gmail.com>
2025-03-11 19:07:08 +0100

    mapreduce

---
+++ Mapreduce/ejerci.py
@@ -0,0 +1,20 @@
+from mrjob.job import MRJob
+import json
+
+class WordCount(MRJob):
+    
+    def mapper(self,_,line):
+        # ejecuta linea a linea
+        text = json.loads(line)
+        for word in text["reviewText"].split():
+            yield (word, 1)
+        
+    
+    def reducer(self,word,count):
+        # sumar los valores
+        yield (word, sum(count))
+
+
+if __name__=="__main__":
+    WordCount.run()
+    
